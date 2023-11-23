be9b235af377d2500d0aea7085fb86b1f9b61c61
Ichi Kanaya <kanaya@pineapple.cc>
2023-11-23 19:17:57 +0900

    now counts up white pixels.

--- color-histogram.py
+++ color-histogram.py
@@ -12,8 +12,11 @@ TH_DEFAULT = 50
 
 parser = argparse.ArgumentParser(description='Create histogram of hue from images.')
 parser.add_argument('image', metavar='IMG', nargs='+', help='image file')
-parser.add_argument('--th', dest='threshold', metavar='TH', choices=range(0, 255), default=TH_DEFAULT,
-					 help='threshold (default: {})'.format(TH_DEFAULT))
+parser.add_argument('--threshold', dest='threshold', metavar='TH', choices=range(0, 255),
+					action='store', default=TH_DEFAULT,
+					help='threshold (default: {})'.format(TH_DEFAULT))
+parser.add_argument('--show-histogram', dest='show_histogram',
+					action='store_const', default=False, const=True, help='show histogram')
 args = parser.parse_args()
 
 img_name = args.image[0]
@@ -25,19 +28,28 @@ img_size = img_height * img_width
 img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 img_s = img_hsv[..., 1]
 ret, img_s_th = cv2.threshold(img_s, th, 255, cv2.THRESH_BINARY)
-cv2.imshow("saturation", img_s_th)
+# cv2.imshow("saturation", img_s_th)
 
-hue_histogram = cv2.calcHist([img_hsv], [0], img_s_th, [N_BINS], [0, 180])
+hue_histogram = cv2.calcHist([img_hsv], channels=[0], mask=img_s_th, histSize=[N_BINS], ranges=[0, 180])
 hue_histogram_v = np.ravel(hue_histogram)
 hue_histogram_v /= img_size
+
+img_v = img_hsv[..., 2]
+value_histogram = cv2.calcHist([img_hsv], channels=[2], mask=None, histSize=[4], ranges=[0, 256])
+value_histogram_v = np.ravel(value_histogram)
+value_histogram_v /= img_size
+
+the_histogram = np.append(hue_histogram_v, value_histogram_v[3])
 
 csv_name = Path(img_name).stem + '.csv'
 csv = open(csv_name, "w")
 for i in range(N_BINS):
-	print("{}, {}".format(i, hue_histogram_v[i]), file=csv)
+	print("{}, {}".format(i, the_histogram[i]), file=csv)
 csv.close()
 
-color_list = [colorsys.hsv_to_rgb(h / N_BINS, 1.0, 1.0) for h in range(N_BINS)]
-fig, ax = plt.subplots()
-ax.bar(np.array([i for i in range(N_BINS)]), hue_histogram_v, color=color_list)
-plt.show()
+if args.show_histogram:
+	color_list = [colorsys.hsv_to_rgb(h / N_BINS, 1.0, 1.0) for h in range(N_BINS)]
+	color_list.append([0.75, 0.75, 0.75])
+	fig, ax = plt.subplots()
+	ax.bar(np.array([i for i in range(N_BINS + 1)]), the_histogram, color=color_list)
+	plt.show()
