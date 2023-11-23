1b80d6813e2ebc3c699275e7f2f6cad50beb54c5
Ichi Kanaya <kanaya@pineapple.cc>
2023-11-23 20:02:02 +0900

    now accepts multiple images.

--- color-histogram.py
+++ color-histogram.py
@@ -21,39 +21,33 @@ parser.add_argument('--show-mask', dest='show_mask', action='store_const', defau
 					help='show mask image')
 args = parser.parse_args()
 
-img_name = args.image[0]
 th = args.threshold
 
-img = cv2.imread(img_name)
-img_height, img_width = img.shape[:2]
-img_size = img_height * img_width
-img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-img_s = img_hsv[..., 1]
-ret, img_s_th = cv2.threshold(img_s, th, 255, cv2.THRESH_BINARY)
-
-if args.show_mask:
-	cv2.imshow("saturation", img_s_th)
-
-hue_histogram = cv2.calcHist([img_hsv], channels=[0], mask=img_s_th, histSize=[N_BINS], ranges=[0, 180])
-hue_histogram_v = np.ravel(hue_histogram)
-hue_histogram_v /= img_size
-
-img_v = img_hsv[..., 2]
-value_histogram = cv2.calcHist([img_hsv], channels=[2], mask=None, histSize=[4], ranges=[0, 256])
-value_histogram_v = np.ravel(value_histogram)
-value_histogram_v /= img_size
-
-the_histogram = np.append(hue_histogram_v, value_histogram_v[3])
-
-csv_name = Path(img_name).stem + '.csv'
-csv = open(csv_name, "w")
-for i in range(N_BINS):
-	print("{}, {}".format(i, the_histogram[i]), file=csv)
-csv.close()
-
-if args.show_histogram:
-	color_list = [colorsys.hsv_to_rgb(h / N_BINS, 1.0, 1.0) for h in range(N_BINS)]
-	color_list.append([0.75, 0.75, 0.75])
-	fig, ax = plt.subplots()
-	ax.bar(np.array([i for i in range(N_BINS + 1)]), the_histogram, color=color_list)
-	plt.show()
+for img_name in args.image:
+	img = cv2.imread(img_name)
+	img_height, img_width = img.shape[:2]
+	img_size = img_height * img_width
+	img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	img_s = img_hsv[..., 1]
+	ret, img_s_th = cv2.threshold(img_s, th, 255, cv2.THRESH_BINARY)
+	if args.show_mask:
+		cv2.imshow("saturation", img_s_th)
+	hue_histogram = cv2.calcHist([img_hsv], channels=[0], mask=img_s_th, histSize=[N_BINS], ranges=[0, 180])
+	hue_histogram_v = np.ravel(hue_histogram)
+	hue_histogram_v /= img_size
+	img_v = img_hsv[..., 2]
+	value_histogram = cv2.calcHist([img_hsv], channels=[2], mask=None, histSize=[4], ranges=[0, 256])
+	value_histogram_v = np.ravel(value_histogram)
+	value_histogram_v /= img_size
+	the_histogram = np.append(hue_histogram_v, value_histogram_v[3])
+	csv_name = Path(img_name).stem + '.csv'
+	csv = open(csv_name, "w")
+	for i in range(N_BINS):
+		print("{}, {}".format(i, the_histogram[i]), file=csv)
+	csv.close()
+	if args.show_histogram:
+		color_list = [colorsys.hsv_to_rgb(h / N_BINS, 1.0, 1.0) for h in range(N_BINS)]
+		color_list.append([0.75, 0.75, 0.75])
+		fig, ax = plt.subplots()
+		ax.bar(np.array([i for i in range(N_BINS + 1)]), the_histogram, color=color_list)
+		plt.show()
