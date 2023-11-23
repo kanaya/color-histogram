1424c69c8efd20d04ffb41af63a437093a2fff42
Ichi Kanaya <kanaya@pineapple.cc>
2023-11-23 18:42:43 +0900

    argparse.

--- color-histogram.py
+++ color-histogram.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
@@ -7,20 +8,18 @@ from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 from pathlib import Path
 
 N_BINS = 72
-TH = 50
+TH_DEFAULT = 50
 
-args = sys.argv
+parser = argparse.ArgumentParser(description='Create histogram of hue from images.')
+parser.add_argument('image', metavar='IMG', nargs='+', help='image file')
+parser.add_argument('--th', dest='threshold', metavar='TH', choices=range(0, 255), default=TH_DEFAULT,
+					 help='threshold (default: {})'.format(TH_DEFAULT))
+args = parser.parse_args()
 
-if len(args) <= 1:
-	print("usage: python color_histogram.py IMAGE_FILE [threshold]\nthreshold: 0-255 (default: 50)")
-	exit()
+img_name = args.image[0]
+th = args.threshold
 
-if len(args) <= 2:
-	th = TH
-else:
-	th = int(args[2])
-
-img = cv2.imread(args[1])
+img = cv2.imread(img_name)
 img_height, img_width = img.shape[:2]
 img_size = img_height * img_width
 img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -32,7 +31,7 @@ hue_histogram = cv2.calcHist([img_hsv], [0], img_s_th, [N_BINS], [0, 180])
 hue_histogram_v = np.ravel(hue_histogram)
 hue_histogram_v /= img_size
 
-csv_name = Path(args[1]).stem + '.csv'
+csv_name = Path(img_name).stem + '.csv'
 csv = open(csv_name, "w")
 for i in range(N_BINS):
 	print("{}, {}".format(i, hue_histogram_v[i]), file=csv)
